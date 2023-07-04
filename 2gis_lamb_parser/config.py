b90b1c63bd3d6c3f087c743930445e862d6baa59
SaymanSmit <56193531+SaymanSmit@users.noreply.github.com>
2023-07-04 15:04:17 -0400

    Delete 2gis_lamb_parser directory

--- 2gis_lamb_parser/config.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-opts = Options()
-opts.add_argument("user-agent=whatever you want")
-
-driver = webdriver.Chrome(chrome_options=opts)
